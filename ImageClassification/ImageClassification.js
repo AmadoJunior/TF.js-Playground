9073e44eaf071d65e255f7d4262af7cbb8e52116
AmadoJunior <amadoelrepalol@gmail.com>
2022-02-22 00:36:36 -0500

    Final

--- ImageClassification/ImageClassification.js
+++ ImageClassification/ImageClassification.js
@@ -239,9 +239,63 @@ function getIrisData(testSplit){
     })
 }
 
+async function trainModel(xTrain, yTrain, xTest, yTest){
+    const model = tf.sequential();
+    const learningRate = .01;
+    const numberOfEpochs = 40;
+    const optimizer = tf.train.adam(learningRate);
+
+    model.add(tf.layers.dense({
+        units:10, activation: "sigmoid", inputShape: [xTrain.shape[1]]
+    }));
+    model.add(tf.layers.dense({
+        units:3, activation: "softmax"
+    }));
+    model.compile({
+        optimizer: optimizer,
+        loss: "categoricalCrossentropy", metrics: ["accuracy"]
+    });
+
+    const history = await model.fit(xTrain, yTrain, {
+        epochs: numberOfEpochs, validationData: [xTest, yTest],
+        callbacks: {
+            onEpochEnd: async (epoch, logs) => {
+                console.log("Epoch: " + epoch + "Logs:" + logs.loss);
+                await tf.nextFrame();
+            },
+        }
+    });
+    return model;
+}
+
 async function doIris(){
     const [xTrain, yTrain, xTest, yTest] = getIrisData(.2);
-    console.log(xTrain);
+    model = await trainModel(xTrain, yTrain, xTest, yTest);
+
+    const input = tf.tensor2d([5.8, 2.7, 5.1, 1.9], [1, 4]);
+    const prediction = model.predict(input);
+    alert(prediction);
+
+    const predictionWithArgMax = model.predict(input).argMax(-1).dataSync();
+    alert(predictionWithArgMax);
+
+    const xData = xTest.dataSync();
+    const yTrue = yTest.argMax(-1).dataSync();
+
+    const predictions = await model.predict(xTest);
+    const yPred = predictions.argMax(1).dataSync();
+
+    var correct = 0;
+    var wrong = 0;
+
+    for(let i = 0; i < yTrue.length; i++){
+        if(yTrue[i] == yPred[i]){
+            correct++;
+        } else {
+            wrong++;
+        }
+    }
+    alert("Prediction error rate: " + (wrong / yTrue.length));
 }
 
 doIris();
